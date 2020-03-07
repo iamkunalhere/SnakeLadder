90b3c497bc8e32781f0f5765e3dca9aa86f11a85
kunal jadhav <iamkunalhere@gmail.com>
2020-03-07 18:42:50 +0530

    Repeat till wining position is 100

--- SnakeAndLadder.sh
+++ SnakeAndLadder.sh
@@ -1,3 +1,4 @@
+
 #!/bin/bash -x
 #Problem Statement : Snake and Ladder Game.
 #Discription : This problem simulates a Snake and Ladder Game.
@@ -18,17 +19,26 @@ startPosition=0
 newPosition=0
 
 function userChoice() {
-	Operation=$( option )
-	case $Operation in
-		"1")
-		newPosition=0
-		;;
-		"2")
-		newPosition=$(($newPosition+$( random )))
-		;;
-		"3")
-		newPosition=$(($newPosition-$( random )))
-		;;
-	esac
+	while [[ $newPosition -lt 100 ]]
+	 do
+		Operation=$( option )
+		case $Operation in
+			"1")
+			newPosition=$newPosition
+			;;
+			"2")
+			newPosition=$(($newPosition+$( random )))
+			;;
+			"3")
+			newPosition=$(($newPosition-$( random )))
+			if [[ $newPosition -lt 0 ]]
+				then
+					newPosition=0
+			fi
+			;;
+		esac
+		echo $newPosition
+	 done
 }
-
+userChoice
+echo $newPosition
