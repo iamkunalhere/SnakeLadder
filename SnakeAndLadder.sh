6805b2b8a49e03b8d21510d0173591b1f88312ff
kunal jadhav <iamkunalhere@gmail.com>
2020-03-07 17:27:34 +0530

    Start with position 0

---
+++ SnakeAndLadder.sh
@@ -0,0 +1,7 @@
+#!/bin/bash -x
+#Problem Statement : Snake and Ladder Game.
+#Discription : This problem simulates a Snake and Ladder Game.
+#Author : Kunal Jadhav
+#Date : 7 March 2020
+
+startPosition=0
