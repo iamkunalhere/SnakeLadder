4ed76f9a5a75f68bd3d2bee484d1c833f693d087
kunal jadhav <iamkunalhere@gmail.com>
2020-03-07 18:14:55 +0530

    Player checks for a option

--- SnakeAndLadder.sh
+++ SnakeAndLadder.sh
@@ -8,5 +8,27 @@ function random() {
 	state=$((RANDOM%6+1))
 	echo $state
 }
+
+function option() {
+	optionState=$((RANDOM%3+1))
+	echo $optionState
+}
+
 startPosition=0
-random
+newPosition=0
+
+function userChoice() {
+	Operation=$( option )
+	case $Operation in
+		"1")
+		newPosition=0
+		;;
+		"2")
+		newPosition=$(($newPosition+$( random )))
+		;;
+		"3")
+		newPosition=$(($newPosition-$( random )))
+		;;
+	esac
+}
+
