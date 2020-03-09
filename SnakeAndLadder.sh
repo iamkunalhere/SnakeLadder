039d00d3a58e448df9037abe240813a720dc6530
kunal jadhav <iamkunalhere@gmail.com>
2020-03-09 13:20:08 +0530

    Updated conditions

--- SnakeAndLadder.sh
+++ SnakeAndLadder.sh
@@ -5,7 +5,6 @@
 #Date : 7 March 2020
 
 function random() {
-	((diceCounter++))
 	state=$((RANDOM%6+1))
 	echo $state
 }
@@ -15,10 +14,9 @@ function option() {
 	echo $optionState
 }
 
-newPosition=0
-prevPosition=0
-
 function userChoice() {
+	newPosition=0
+	prevPosiion=0
 	diceCounter=0
 	while [[ $newPosition -lt 100 ]]
 	 do
@@ -31,21 +29,21 @@ function userChoice() {
 			"2")
 			prevPosition=$newPosition
 			newPosition=$(($newPosition+$( random )))
+			((diceCounter++))
 			if [[ $newPosition -gt 100 ]]
 				then
 					newPosition=$prevPosition
 			fi
-			((diceCounter++))
 #			echo "position after die roll $newPosition"
 			;;
 			"3")
 			prevPosition=$newPosition
 			newPosition=$(($newPosition-$( random )))
+			((diceCounter++))
 			if [[ $newPosition -lt 0 ]]
 				then
 					newPosition=0
 			fi
-			((diceCounter++))
 #			echo "position after die roll $newPosition"
 			;;
 		esac
@@ -55,11 +53,14 @@ function userChoice() {
 
 read -p "Enter Player1 name: " player1
 read -p "Enter Player2 name: " player2
+
 player1Rolls=$( userChoice )
 player2Rolls=$( userChoice )
+
 echo "$player1Rolls times $player1 rolls the dice"
 echo "$player2Rolls times $player2 rolls the dice"
-if [[ $player1Rolls < $player2Rolls ]]
+
+if [[ $player1Rolls -lt $player2Rolls ]]
  then
 	echo "Winner is $player1"
  else
